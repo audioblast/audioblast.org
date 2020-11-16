241ad6c457d80d4c4d73282203feea483181667a
Ed Baker <edwbaker@gmail.com>
2020-11-16 15:31:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <html>
 <head>
-<title>audioBLAST!</title>
+  <title>audioBLAST!</title>
 </head>
 
 <body>
-<h1>audioBLAST!</h1>
-<p>Wleocme to audioBLAST!</p>
+  <h1>audioBLAST!</h1>
+  <p>Welcome to audioBLAST!</p>
+  <ul>
+    <li><a href="https://audioblast.org">Home</a></li>
+    <li>ann-o-mate</li>
+    <li>API</li>
+    <li><a href="https://cdn.audioblast.org">CDN</a></li>
+  </ul>
 </body>
 </html>
