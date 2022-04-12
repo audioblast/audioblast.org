217a62a7f56f0e257aa875340c16b4daee28f70e
Ed Baker <edwbaker@gmail.com>
2022-04-12 09:51:08 +0100

    Initial about page.

--- about.php
+++ about.php
@@ -6,46 +6,12 @@ error_reporting(E_ALL);
 <html>
 
 <head>
-<title>audioBLAST! Recordings</title>
-<link rel="stylesheet" href="https://cdn.audioblast.org/ab-api.css">
-<link href="https://cdn.audioblast.org/tabulator/dist/css/tabulator.min.css" rel="stylesheet">
-<script type="text/javascript" src="https://cdn.audioblast.org/tabulator/dist/js/tabulator.min.js"></script>
-<script type="text/javascript" src="https://cdn.audioblast.org/ab-tabulator.js"></script>
+  <title>About audioBLAST!</title>
+  <link rel="stylesheet" href="https://cdn.audioblast.org/ab-api.css">
 </head>
 
 <body>
-<?php
-if (isset($_GET["page"])) {
-  $current = $_GET["page"];
-} else {
-  $current = "recordings";
-}
-?>
+  <h1>About audioBLAST!</h1>
+  <p>Coming soon.</p>
 
-<h1><?php print($current); ?></h1>
-
-<ul class='ulhoriz'>
-<?php
-$types = json_decode(file_get_contents("https://api.audioblast.org/standalone/modules/list_modules/?category=data&output=nakedJSON"));
-foreach ($types as $type) {
-  print("<li><a href='https://audioblast.org/?page=".$type->name."'>".$type->hname."</a></li>");
-}
-?>
-</ul>
-
-<div id="module-info">
-<?php
-  $minfo = json_decode(file_get_contents("https://api.audioblast.org/standalone/modules/module_info/?output=nakedJSON&module=".$current));
-  print($minfo->desc);
-?>
-</div>
-
-<div id="data-table">
-</div>
-
-</body>
-
-<script>
-generateTabulator("#data-table", "<?php print($current); ?>");
-</script>
 </html>
