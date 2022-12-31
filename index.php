0cb41a1c4b442eb4146f4920b68ccaa904b27e14
Ed Baker <edwbaker@gmail.com>
2022-12-31 13:11:06 +0000

    Fix formatting.

--- index.php
+++ index.php
@@ -41,7 +41,6 @@ if ($_SERVER['SERVER_NAME'] == 'ab.acousti.cloud') {
     <p>AudioBlast is a project to collect and analyse sound files and data from around the world to make a bioacoustic discovery and search engine.</p>
     <p>This website uses the <a href="https://api.audioblast.org">audioBlast API</a> which you can use to create your own projects.</p>
     <?php
-    include("home.php");
   } else {
     print("<ul class='ulhoriz'>");
     $types = json_decode(
@@ -49,18 +48,25 @@ if ($_SERVER['SERVER_NAME'] == 'ab.acousti.cloud') {
     foreach ($types as $type) {
       print("<li><a href='https://audioblast.org/?page=".$type->name."'>".$type->hname."</a></li>");
     }
-    ?>
-    </ul>
-    <div id="data-table">
-    </div>
-    <script>
-      generateTabulator("#data-table", "<?php print($current_page); ?>");
-    </script>
-    <?php
+    print("</ul>");
   }
   ?>
   </div>
 </div>
 
+<?php
+if ($current_page == "home") {
+  include("home.php");
+} else {
+  ?>
+  <div id="data-table">
+  </div>
+  <script>
+    generateTabulator("#data-table", "<?php print($current_page); ?>");
+  </script>
+  <?php
+}
+?>
 </body>
+
 </html>
