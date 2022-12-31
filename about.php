361e310c16bf5f0d2bf3597d30716c8962bd93be
Ed Baker <edwbaker@gmail.com>
2022-12-31 22:27:55 +0000

    Simplify.

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-  <title>About audioBLAST!</title>
+  <title>About audioBlast</title>
   <link rel="stylesheet" href="/ab-api.css">
 </head>
 
@@ -10,8 +10,7 @@
     <a href="/"><img src="https://cdn.audioblast.org/audioblast_flash.png" class="audioblast-flash" /></a>
     <h1>About audioBLAST!</h1>
     <div id="menu">
-      <p>Audioblast is a project to collect and analyse sound files and data from around the world to make a bioacoustic discovery and search engine.</p>
-      <p>This website uses the <a href="https://api.audioblast.org">audioBLAST API</a> which you can use to create your own projects.</p>
+      <?php include("includes/welcome.php"); ?>
     </div>
   </div>
 
@@ -19,12 +18,19 @@
     <div class="feature">
       <h2>Development</h2>
       <h3>Automated Acoustic Observatories</h3>
+      <p>The audioBlast infrastructure was originally implemented to internally support the Leverhulme
+         Trust funded <i>Automated Acoustic Observatories</i> project at the University of York.</p>
       <h3>Urban Nature Project</h3>
+      <p>Part of the current development cycle is supported by the <i>Urban Nature Project</i> at <thead>
+         Natural History Museum, London.</p>
       <h3>GitHub</h3>
+      <p>You can contribute to the project at the <a href="https://github.com/audioblast">audioBlast
+        GitHub</a>.</p>
     </div>
 
     <div class="feature">
       <h2>Infrastructure</h2>
+      <p>Computing infrastructure is currently provided by the Natural History Museum, London.</p>
     </div>
 
     <div class="feature">
@@ -38,7 +44,5 @@
       <h3>zcjs</h3>
       <h3>PhyMoji</h3>
     </div>
-
   </div>
-
 </html>
--- index.php
+++ index.php
@@ -36,12 +36,8 @@ if ($_SERVER['SERVER_NAME'] == 'ab.acousti.cloud') {
   <div id="menu">
   <?php
   if ($current_page == "home") {
+    include("includes/welcome.php");
     ?>
-    <h2>Welcome to audioBlast!</h2>
-    <p>AudioBlast is a project to collect and analyse sound files and data from around the world
-       to make a bioacoustic discovery and search engine.</p>
-    <p>This website uses the <a href="https://api.audioblast.org">audioBlast API</a> which you
-       can use to create your own projects. Read more <a href="about.php">about audioBlast</a>.</p>
     </div></div>
 <?php
   include("home.php");
