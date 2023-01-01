61d6e3e802bade68877466818f6a373621ee4294
Ed Baker <edwbaker@gmail.com>
2023-01-01 15:09:09 +0000

    Refactor rosetta

--- search_plugins/rosetta.js
+++ search_plugins/rosetta.js
@@ -1,16 +1,16 @@
 const rosetta = {
-    name:"Rosetta",
-    preParse(string, core) {
-      if(string == "🦗")  {
-        core.consoleLog(this.name, "Interpreted 🦗 as Orthoptera.");
-        return("Orthoptera");
-      }
-      return(string);
-    },
-    canParse(string, core) {
-      core.parsed(this.name, false);
-    },
-    searchSuggest() {
-      return(["🦗"]);
+  name:"Rosetta",
+  preParse(string, core) {
+    if(string == "🦗")  {
+      core.consoleLog(this.name, "Interpreted 🦗 as Orthoptera.");
+      return("Orthoptera");
     }
+    return(string);
+  },
+  canParse(string, core) {
+    core.parsed(this.name, false);
+  },
+  searchSuggest() {
+    return(["🦗"]);
   }
+}
