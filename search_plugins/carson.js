dcee6dd698bb03ae8bdc13d61ee9960e512d32eb
Ed Baker <edwbaker@gmail.com>
2023-01-04 18:44:16 +0000

    Concurrency

--- search_plugins/carson.js
+++ search_plugins/carson.js
@@ -1,6 +1,10 @@
 const carson = {
   name:"Carson",
+  query:  Promise.resolve(),
   parse(mode, match, core) {
+    this.query.then(this.doParse(mode, match, core));
+  },
+  doParse(mode, match, core) {
     match = match.toLowerCase();
     const parts = match.split(" ");
     parts.forEach(match => {
@@ -18,9 +22,12 @@ const carson = {
       core.replaceMatch("how far", "Sound propagation distance (m)", this.name);
       return;
     }
-
     if (match.includes("what distance")) {
-        core.replaceMatch("what distance", "Sound propagation distance (m)", this.name);
+      core.replaceMatch("what distance", "Sound propagation distance (m)", this.name);
+      return;
+    }
+    if (match.includes("what frequency")) {
+        core.replaceMatch("what frequency", "Peak Frequency (kHz)", this.name);
         return;
       }
 
