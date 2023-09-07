97331d05c3b95d7701367aa141c1be388cb5f482
Ed Baker <edwbaker@gmail.com>
2023-09-07 15:03:35 +0000

    Documentation

--- audioblast.php
+++ audioblast.php
@@ -1,4 +1,14 @@
-<?php include("includes/init.php"); ?>
+<?php 
+/* 
+audioBlast Search
+
+This page is for the audioBlast search interface. It is a single page app that uses the audioBlast 
+API to search for recordings, annotations and traits.
+*/
+
+// Initial configurtion and quick return if ping page
+include("includes/init.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +18,10 @@
   <link rel="stylesheet" href="https://cdn.audioblast.org/tabulator/dist/css/tabulator.min.css">
   <script type="text/javascript" src="https://cdn.audioblast.org/tabulator/dist/js/tabulator.min.js"></script>
   <script type="text/javascript" src="/ab-tabulator.js"></script>
-  <?php include("includes/load_search_js.php"); ?>
+  <?php 
+    //Load the search plugins
+    include("includes/load_search_js.php"); 
+  ?>
 </head>
 
 <body>
@@ -36,6 +49,7 @@
   <div id="contentContainer" class="feature-container"></div>
 
   <script>
+    //Start the search once the page has loaded
     document.addEventListener("DOMContentLoaded", function(event) {
       searchAB.init();
     });
