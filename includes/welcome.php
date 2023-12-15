5bcc54480066d87f88bb4fc94510a805fc136dd3
Ed Baker <edwbaker@gmail.com>
2023-12-15 16:20:01 +0000

    Updates page.

--- includes/welcome.php
+++ includes/welcome.php
@@ -2,4 +2,5 @@
     <p>AudioBlast is a project to collect and analyse sound files and data from around the world
        to make a bioacoustic discovery and search engine.</p>
     <p>This website uses the <a href="https://api.audioblast.org">audioBlast API</a> which you
-       can use to create your own projects. Read more <a href="about.php">about audioBlast</a>.</p>
+       can use to create your own projects. Read more <a href="about.php">about audioBlast</a> or
+       check ut our <a href="updates.php">latest updates</a>.</p>
---
+++ updates.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+  <title>audioBlast Updates</title>
+  <link rel="stylesheet" href="/ab-api.css">
+</head>
+
+<body>
+  <div id="title">
+    <a href="/"><img src="https://cdn.audioblast.org/audioblast_flash.png" class="audioblast-flash" /></a>
+    <h1>audioBLAST Updates</h1>
+    <div id="menu">
+      <?php include("includes/welcome.php"); ?>
+    </div>
+  </div>
+
+  <div class="feature-container">
+    <div class="feature">
+      <h3>Week of 10th December 2023</h3>
+      <ul>
+        <li>Soundscape recordings can now be associated with a deployment.</li>
+        <li>Soundscapes view shows data from deployments.</li>
+        <li>First data from BirdNET is now in the system.</li>
+        <li>There is now an updates page.</li>
+      </ul>
+    </div>
+
+    </div>
+  </div>
+</html>
